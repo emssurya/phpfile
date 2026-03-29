f7a2e7782e09bf8b6330d49541ba4b4abde8013f
emssurya <emssurya56@gmail.com>
2026-03-29 15:35:24 +0530

    test

--- php/QUADRATIC EQUATION.php
+++ php/QUADRATIC EQUATION.php
@@ -1,36 +1,27 @@
-QUADRATIC EQUATION
-
-
 <?php
 $a = 1;
-$b = -3; $c
-= 2;
+$b = -3;
+$c = 2;
+
 $D = ($b * $b) - (4 * $a * $c);
+
 echo "<h2>Roots of Quadratic Equation</h2>";
-if($D > 0)
-{
-$r1 = (-$b + sqrt($D)) / (2 * $a);
-$r2 = (-$b - sqrt($D)) / (2 * $a);
-echo "Roots are Real and Distinct<br>";
-echo "Root 1 = ".$r1."<br>"; echo
-"Root 2 = ".$r2;
-}
-elseif($D == 0)
-{
-$r = (-$b) / (2 * $a);echo "Roots are Real and Equal<br>";
-echo "Root = ".$r;
-} else
-{
-echo
-"Roo
-ts are
-Imag
-inary
-";
+
+if ($D > 0) {
+    $r1 = (-$b + sqrt($D)) / (2 * $a);
+    $r2 = (-$b - sqrt($D)) / (2 * $a);
+
+    echo "Roots are Real and Distinct<br>";
+    echo "Root 1 = " . $r1 . "<br>";
+    echo "Root 2 = " . $r2;
+
+} elseif ($D == 0) {
+    $r = (-$b) / (2 * $a);
+
+    echo "Roots are Real and Equal<br>";
+    echo "Root = " . $r;
+
+} else {
+    echo "Roots are Complex (Imaginary)";
 }
 ?>
-
-
-
-
-
